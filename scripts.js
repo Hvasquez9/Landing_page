34ef2c0f3b42cf3e5561f996f37564ed8bd3ac16
Hvasquez9 <hectorvasquezcarrilllo@gmail.com>
2023-05-16 18:10:07 -0500

    mycommit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function mostrarInformacion(){
+    var nombreUsuario = document.getElementById("nombre").value;
+    var correoUsuario = document.getElementById("correo").value;
+    // alert(nombreUsuario);
+    // alert(correoUsuario);
+    console.log(nombreUsuario);
+    console.log(correoUsuario);
+
+}
